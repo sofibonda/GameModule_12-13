b1f838fb40abe9d55ed9d947d26ac9f79633055e
sofibonda <sofi.pumpkin@gmail.com>
2026-01-08 22:06:51 +0100

    game first try

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class Coin : MonoBehaviour
 {
     [SerializeField] private GameObject _coin;
 
+    private static int coinCount = 0;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            Debug.Log("Вы собрали монету!");
+            coinCount++;
+            Debug.Log($"Вы собрали {coinCount} монету!");
             Destroy(gameObject);
         }
     }
