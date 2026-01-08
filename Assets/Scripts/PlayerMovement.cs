f7aae5e15a206111c45487cbcdde3e5d9c7937a9
sofibonda <sofi.pumpkin@gmail.com>
2026-01-08 21:18:28 +0100

    add player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _moveForce;
+    [SerializeField] private float _jumpForce;
+
+    [SerializeField] private GameObject _groundChecker;
+
+    [SerializeField] private Rigidbody _rb;
+
+    private bool _isGrounded;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        float x = Input.GetAxis("Horizontal"); 
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(x, 0f, z);
+
+        _rb.AddForce(moveDirection * _moveForce);
+
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+        {
+            _rb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+            _isGrounded = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+            _isGrounded = false;
+    }
+}
